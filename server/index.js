af5a0fcf1eb2f3a96a723eaeec2242cdf4d72fe1
OlegVasyliev <49238305+OlegVasyliev@users.noreply.github.com>
2020-12-23 02:40:01 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://oleh123:1234@cluster0.cu2jf.mongodb.net/oleh123?retryWrites=true&w=majority';
+const CONNECTION_URL = '';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
